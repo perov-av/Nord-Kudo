a4aeb178efc2b1d0dd0c63f2701fabe2ca082eaa
perov-av <43490882+perov-av@users.noreply.github.com>
2018-11-04 20:32:13 +0300

    04-11

--- js/common.js
+++ js/common.js
@@ -1,12 +1,27 @@
 $(document).ready(function() {
+  
 
   $(".itemPopup").magnificPopup({
-    type : 'image',
-    gallery : {
-      enabled : true
-    },
-    removalDelay: 300,
-    mainClass: 'mfp-fade'
+  type : 'image',
+  gallery : {
+    enabled : true
+  },
+  removalDelay: 300,
+  mainClass: 'mfp-fade'
+  });
+
+  $(".home-gallery-wrap").each(function() {
+    $(this).magnificPopup({
+      
+      delegate: 'a',
+      type : 'image',
+      tLoading: '',
+      gallery : {
+        enabled : true
+      },
+      removalDelay: 300,
+      mainClass: 'mfp-fade-gallary'
+    });
   });
 
   $("#form").submit(function () {
@@ -23,12 +38,27 @@ $(document).ready(function() {
   });
 });
 
+
 $(function() {
+  
+  $(".item-card").height('auto').equalHeights();
 
   $(".mouse-icon").click(function() {
     $("html, body").animate({
       scrollTop : $(".s-adv").offset().top
     }, 800);
   });
+});
 
-})
+
+$(window).load(function() {
+
+  $(".overlay h1.anim-left").animated("fadeInLeft", "fadeInRight");
+  $(".overlay h1.anim-right").animated("fadeInRight", "fadeInLeft");
+  $(".m_Text h1").animated("flip", "flipInY");
+  $(".m_Wrap h1.come").animated("lightSpeedIn", "lightSpeedOut");
+  $(".m_Wrap h1.anim-down").animated("fadeInDown", "fadeInUp");
+  $(".m_Wrap h1.bounce").animated("bounceIn", "bounceIn");
+  $(".item_text h2.anim-right").animated("fadeInRight", "fadeInLeft");
+   $(".item_text h2.anim-left").animated("fadeInLeft", "fadeInRight");
+});
